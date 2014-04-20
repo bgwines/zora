91f617c31e96fca00c251e6a8b56b687af1bfff4
Brett Wines <brettwines@gmail.com>
2014-04-19 18:37:40 -0700

    Package built!

---
+++ dist/build/autogen/Paths_HLib.hs
@@ -0,0 +1,36 @@
+module Paths_HLib (
+    version,
+    getBinDir, getLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+catchIO = Exception.catch
+
+
+version :: Version
+version = Version {versionBranch = [1,0,0], versionTags = []}
+bindir, libdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/Users/brett/.cabal/bin"
+libdir     = "/Users/brett/.cabal/lib/x86_64-osx-ghc-7.6.3/HLib-1.0.0"
+datadir    = "/Users/brett/.cabal/share/x86_64-osx-ghc-7.6.3/HLib-1.0.0"
+libexecdir = "/Users/brett/.cabal/libexec"
+sysconfdir = "/Users/brett/.cabal/etc"
+
+getBinDir, getLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "HLib_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "HLib_libdir") (\_ -> return libdir)
+getDataDir = catchIO (getEnv "HLib_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "HLib_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "HLib_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
